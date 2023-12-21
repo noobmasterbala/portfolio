7e33b6bfba02990c5060c8b869c99d0b44c63024
Balaji Rajaguru Rajakumar <rrbalaji10@gmail.com>
2023-12-21 14:35:52 -0700

    minor changes

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,18 +1,36 @@
-document.getElementById('emailButton').onclick = function() {
-    document.getElementById('emailModal').style.display = "block";
-  }
+// Wait for the DOM to fully load
+document.addEventListener('DOMContentLoaded', function() {
   
-  document.getElementsByClassName('close')[0].onclick = function() {
-    document.getElementById('emailModal').style.display = "none";
-  }
+    // Get the modal
+    var modal = document.getElementById('emailModal');
   
-  window.onclick = function(event) {
-    if (event.target == document.getElementById('emailModal')) {
-      document.getElementById('emailModal').style.display = "none";
+    // Get the button that opens the modal
+    var emailButton = document.getElementById('emailButton');
+  
+    // Get the element that closes the modal
+    var closeButton = document.querySelector('.close'); // Use querySelector to be more specific
+  
+    // When the user clicks the button, open the modal 
+    emailButton.onclick = function() {
+      modal.style.display = "block";
     }
-  }
   
-  document.getElementById('proceedButton').onclick = function() {
-    window.location.href = 'mailto:balajirajagur@gmail.com';
-  }
+    // When the user clicks on (x), close the modal
+    closeButton.onclick = function() {
+      modal.style.display = "none";
+    }
+  
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  
+    // When the user clicks on 'Proceed to Email', open the default email client
+    var proceedButton = document.getElementById('proceedButton');
+    proceedButton.onclick = function() {
+      window.location.href = 'mailto:balajirajagur@gmail.com';
+    }
+  });
   
